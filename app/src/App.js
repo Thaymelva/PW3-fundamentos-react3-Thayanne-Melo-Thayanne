63df6b65b495ace36bed7b7e2c760cc5425d14e6
Thaymelva <128633524+Thaymelva@users.noreply.github.com>
2024-02-29 22:44:46 -0300

    intro state

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,55 @@
-import dadosJSON from "./dados.json"
-import Livro from './components/livro';
+import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 
 function App() {
+
+  /*ESTADO DE DADO NOME*/
+  const [nome, setNome] = useState("");
+
+  /*ESTADO DE DADOS DE E-MAIL*/
+  const [email, setEmail] = useState("");
+  
+  /*ESTADO DE DADOS DE SENHA*/
+  const [SENHA, setSenha] = useState("");
+
+  function cadastrarUsuario(event){
+    event.preventDefault();
+    // alert('TESTE DE SUBMIT');
+    alert (`NOME:${nome} E-MAIL:${email} SENHA:${SENHA}`);
+  }
   return (
     <div className="App">
-        <Livro dadosJSON={dadosJSON}/>
+
+     <form onSubmit={cadastrarUsuario} className='form'>
+      <h1>FORMULÁRIO DE CADASTRO</h1>
+
+      <input
+      type='text'
+      placeholder='DIGITE SEU NOME AQUI'
+      required
+      value={nome}
+      onChange={(event)=>{setNome(event.target.value)}}
+      />
+
+      <input
+      type='email'
+      placeholder='DIGITE SEU EMAIL AQUI'
+      required
+      value={email}
+      onChange={(event)=>{setEmail(event.target.value)}}
+      />
+
+      <input
+      type='password'
+      placeholder='DIGITE SUA SENHA'
+      required
+      value={SENHA}
+      onChange={(event)=>{setSenha(event.target.value)}}
+      />
+
+      <button type='submit'> CADASTRAR</button>
+     </form>
     </div>
   );
 }
